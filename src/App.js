bb994db8f52f863a5f65a62630b12e3cd666c8b9
Sofi-grl <64422085+Sofi-grl@users.noreply.github.com>
2022-07-06 20:50:41 +0300

    login keycloak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
-function App() {
+import Resources from './components/Resources';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div>
+    <BrowserRouter>
+      <NavBar/>
 
-export default App;
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/resource' element={<Resources/>}/>
+      </Routes>
+
+      <Footer/>
+    </BrowserRouter>
+  </div>
+  )
+}
